097dbf19b285c6619d98222ce1d3612fe77d44a9
ammadkh <ammadhassan104@gmail.com>
2022-04-28 12:38:38 +0500

    Added search feature

--- src/features/characters/components/search-bar.component.js
+++ src/features/characters/components/search-bar.component.js
@@ -2,9 +2,12 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import {Searchbar} from 'react-native-paper';
 
-export const SearchBarComponent = () => {
+export const SearchBarComponent = ({onSearch}) => {
   const [searchQuery, setSearchQuery] = React.useState('');
-  const onChangeSearch = query => setSearchQuery(query);
+  const onChangeSearch = query => {
+    setSearchQuery(query);
+    onSearch(query);
+  };
 
   return (
     <Searchbar
--- src/features/characters/screens/characters.screen.js
+++ src/features/characters/screens/characters.screen.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Text, StyleSheet, View, ScrollView} from 'react-native';
+import {StyleSheet, View, ScrollView} from 'react-native';
 import {SafeViewComponent} from '../../../components/UI/SafeViewComponent';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {SearchBarComponent} from '../components/search-bar.component';
@@ -7,48 +7,40 @@ import {Character} from '../components/character.component';
 import {DataTable} from 'react-native-paper';
 
 Icon.loadFont();
-const numberOfItemsPerPageList = [2, 3, 4];
-
-const items = [
-  {
-    key: 1,
-    name: 'Page 1',
-  },
-  {
-    key: 2,
-    name: 'Page 2',
-  },
-  {
-    key: 3,
-    name: 'Page 3',
-  },
-];
 export const Characters = () => {
   const [characters, setCharacters] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
   const [page, setPage] = React.useState(0);
   const [totalCount, setTotalCount] = useState(1);
   const [numberOfItemsPerPage, onItemsPerPageChange] = React.useState(10);
   const from = page * numberOfItemsPerPage;
   const to = Math.min((page + 1) * numberOfItemsPerPage, totalCount);
 
+  const onSearchHandler = query => {
+    setSearchQuery(query);
+  };
+
   // Need to use react query, just fetching data right now
   useEffect(() => {
-    fetch(`https://swapi.dev/api/people/?page=${page + 1}`, {
-      method: 'Get',
-      headers: {
-        'Content-Type': 'application/json',
+    fetch(
+      `https://swapi.dev/api/people/?page=${page + 1}&search=${searchQuery}`,
+      {
+        method: 'Get',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    })
+    )
       .then(response => response.json())
       .then(data => {
         setCharacters(data.results);
         setTotalCount(data.count);
       });
-  }, [page]);
+  }, [page, searchQuery]);
   return (
     <SafeViewComponent>
       <View style={styles.container}>
-        <SearchBarComponent />
+        <SearchBarComponent onSearch={onSearchHandler} />
         <ScrollView>
           {characters?.map(character => (
             <Character character={character} />
